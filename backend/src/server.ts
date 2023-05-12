05167b71a760af542ec2a99310436df6c0d8f309
sijibomi <gbemilesanmi@gmail.com>
2023-05-12 01:59:09 +0100

    chore: express init with built in express middleware

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,10 +1,19 @@
 import Express from "express"
 import { env } from "./config/env"
 
+//NOTE: express init
 const app = Express()
 
+//NOTE: Middleware
+app.use(Express.json())
+app.use(Express.urlencoded({extended:false}))
+
+//NOTE: healthCheck
+app.get("/PING" , (_req,res) => {res.send("PONG")})
 
 
+
+//NOTE: app initialization!!! 
 app.listen(env.PORT , () => {
     console.log("Server is listening at PORT: " , env.PORT ,"...")
 })
