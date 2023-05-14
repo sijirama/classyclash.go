3e499db8516102ce139ea877b6acc212c99495b7
sijibomi <gbemilesanmi@gmail.com>
2023-05-14 01:26:51 +0100

    feat: update user

--- backend/src/controllers/user.controller.ts
+++ backend/src/controllers/user.controller.ts
@@ -3,6 +3,8 @@ import { UserModel } from "../models/User"
 //import { NoteModel } from "../models/Note"
 import asyncHandler from "express-async-handler"
 import bcrypt from "bcrypt"
+import { extend } from "joi"
+import mongoose from "mongoose"
 
 //NOTE: @route GET /users
 //WARN: access private
@@ -42,7 +44,33 @@ export const createNewUser = async(req:Request , res:Response) => {
 
 //NOTE: @route PATCH /users
 //WARN: access private
-export const updateUser = asyncHandler(async(req:Request , res:Response) => {})
+export const updateUser = async(req:Request , res:Response) => {
+    const {id , username , roles , active , password } = req.body
+    if(!id || !username || !Array.isArray(roles) ||!roles.length || typeof active !== "boolean" ){
+        return res.status(400).json({message:"All fields are required!"})
+    }
+    const duplicate = await UserModel.findOne({username}).lean().exec()
+    const user = await UserModel.findById({_id:id}).lean().exec() as typeof duplicate
+    if(!user){
+        return res.status(400).json({message:"User not found!"})
+    }
+
+    if(duplicate && duplicate._id.toString() !== id){
+        return res.status(400).json({message:"Duplicate username!!"})
+    }
+
+    user.username = username
+    user.roles = roles
+    user.active = active
+
+    if(password){
+        user.password = await bcrypt.hash(password , 10)
+    }
+
+    const updatedUser = (user as any).save()
+
+    res.status(201).json({message:`${updatedUser.username} has been updated!`})
+}
 
 //NOTE: @route DELETE /users
 //WARN: access private
