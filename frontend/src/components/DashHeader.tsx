e3e69b89e62a5c9166f81c85db8284dad67274cb
sijibomi <gbemilesanmi@gmail.com>
2023-05-17 00:04:02 +0100

    feat: basic layout

---
+++ frontend/src/components/DashHeader.tsx
@@ -0,0 +1,18 @@
+import {Link} from "react-router-dom"
+
+function DashHeader() {
+  return (
+    <header className="dash-header">
+        <div className="dash-header__container">
+            <Link to="/dash/notes">
+                <h1 className="dash-header__title">techNotes</h1>
+            </Link>
+            <nav className="dash-header__nav">
+
+            </nav>
+        </div>
+    </header>
+  )
+}
+
+export default DashHeader
