0398aee9a5563c3dd95eb18de566f0672a896051
sijibomi <gbemilesanmi@gmail.com>
2023-05-16 23:16:32 +0100

    init: frontend

---
+++ frontend/src/components/Public.tsx
@@ -0,0 +1,13 @@
+import {Link} from "react-router-dom"
+import React from 'react'
+
+function Public() {
+  return (
+    <div>
+        <p>Welocme to the mern playground.</p>
+        <p>i practice ui development and carry out some frontend backend interaction experiments.</p>
+    </div>
+  )
+}
+
+export default Public
