fbdc617121e27ea59c979a2226f5c9a78996e421
sijibomi <gbemilesanmi@gmail.com>
2023-05-19 00:52:04 +0100

    feat: error middleware

--- server/routes/user.routes.ts
+++ server/routes/user.routes.ts
@@ -1,7 +1,8 @@
 import {Router} from "express"
 import * as UserController from "../controllers/user.controller"
+import asyncHandler from "express-async-handler"
 const router = Router()
 
-router.get("/auth" , UserController.authenticateUser)
+router.get("/auth" , asyncHandler(UserController.authenticateUser))
 
 export default router
