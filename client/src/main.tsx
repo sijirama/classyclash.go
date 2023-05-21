6fb7964f0dcf325589f3679abc766774cf5be8f4
sijibomi <gbemilesanmi@gmail.com>
2023-05-21 23:11:01 +0100

    feat: client: login component

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -6,11 +6,13 @@ import "bootstrap/dist/css/bootstrap.min.css"
 import './index.css'
 import HomePage from './pages/HomePage'
 import NotFoundPage from './pages/NotFound'
+import Login from './pages/Login'
 
 const route = createBrowserRouter(
     createRoutesFromElements(
         <Route path='/' element={<App />}>
             <Route index={true} path="/" element={<HomePage />} />
+            <Route path='login' element={<Login />}/>
             <Route path="*" element={<NotFoundPage />}/>
         </Route>
     )
--- client/src/pages/Login.tsx
+++ client/src/pages/Login.tsx
@@ -16,6 +16,39 @@ function Login() {
   return (
     <FormContainer>
         <h1>Sign In</h1>
+        <Form onSubmit={submitHandler}>
+            
+            <Form.Group className='my-2' controlId='email'>
+                <Form.Label>Email</Form.Label>
+                <Form.Control
+                    type='email'
+                    placeholder='Enter your Email Address'
+                    value={email}
+                    onChange= { (e) => setEmail(e.target.value) }
+                ></Form.Control>
+            </Form.Group>
+        
+            <Form.Group className='my-2' controlId='password'>
+                <Form.Label>Password</Form.Label>
+                <Form.Control
+                    type='password'
+                    placeholder='Enter your Password'
+                    value={password}
+                    onChange= { (e) => setPassword(e.target.value) }
+                ></Form.Control>
+            </Form.Group>
+
+            <Button variant="primary" type="submit" className='mt-3'>
+                Sign In
+            </Button>
+
+            <Row className="py-3">
+                <Col>
+                    New Customer <Link to="/register">Register here</Link>
+                </Col>
+            </Row>
+
+        </Form>
     </FormContainer>
   )
 }
