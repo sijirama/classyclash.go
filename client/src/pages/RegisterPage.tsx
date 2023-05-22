b41f62dc074f2e49e2452e2599ff8cdfc5b52b38
sijibomi <gbemilesanmi@gmail.com>
2023-05-22 21:25:07 +0100

    feat: register mutation added

--- client/src/pages/RegisterPage.tsx
+++ client/src/pages/RegisterPage.tsx
@@ -1,7 +1,13 @@
-import React , {useState} from 'react'
+import  {useState , useEffect} from 'react'
 import {Link} from "react-router-dom"
 import {Form , Button , Row , Col} from "react-bootstrap"
 import FormContainer from '../components/FormContainer'
+
+import { useAppDispatch, useAppSelector } from '../app/hooks'
+import { useRegisterMutation } from '../app/slices/userApiSlice'
+import { setCredentials } from '../app/slices/authSlice'
+import { useNavigate } from 'react-router-dom'
+import { toast } from 'react-toastify'
 
 
 
@@ -12,9 +18,33 @@ function RegisterPage() {
     const [password, setPassword] = useState("")
     const [confirmpassword, setConfirmPassword] = useState("")
 
+    const dispatch = useAppDispatch()
+    const navigate = useNavigate()
+    const [register , {isLoading}] = useRegisterMutation()
+    const {userInfo} = useAppSelector((state) => state.auth)
+
+    useEffect(() => {
+            if(userInfo){
+                    navigate("/")
+                }
+        } , [navigate , userInfo ])
+
     const submitHandler = async (e:any) => {
         e.preventDefault()
         console.log("submit" , email , password , name , confirmpassword)
+        if(password !== confirmpassword){
+                toast.error("Make sure passwords match!")
+                return
+        }
+        try {
+           const res = await register({email , password , name}).unwrap()
+           dispatch(setCredentials({...res.user}))
+           toast.success("Welcome to Hell!")
+           navigate("/")
+        } catch (err:any) {
+            toast.error(err?.data?.message || err.error)
+            console.error(err)
+        }
     }
 
   return (
