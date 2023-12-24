92ef7a720e607493fcf2a70bf125ad1576289cc6
sijibomi <gbemilesanmi@gmail.com>
2023-12-24 18:00:29 +0100

    initial

--- server/config/environment.ts
+++
@@ -1,9 +0,0 @@
-import dotenv from "dotenv"
-dotenv.config()
-
-export const env = {
-    PORT: process.env.PORT || 3500,
-    MONGO_URI: process.env.MONGO_URI,
-    JWT_SECRET: process.env.JWT_SECRET,
-    NODE_ENV: process.env.NODE_ENV
-}
