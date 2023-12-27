7f892681bc5923dbb6f7b620d04e0f364e6b03dd
sijibomi <gbemilesanmi@gmail.com>
2023-12-27 03:20:57 +0100

    some slaughter ang shit

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -1,9 +1,18 @@
 import { NextAuthOptions } from "next-auth";
-import {PrismaAdapter} from "@next-auth/prisma-adapter"
+import { PrismaAdapter } from "@next-auth/prisma-adapter";
 import { db } from "./db";
+import GoogleProvider from "next-auth/providers/google";
 
 export const authOptions: NextAuthOptions = {
-    adapter:PrismaAdapter(db),
-    providers: [],
-
+    adapter: PrismaAdapter(db),
+    session: {
+        strategy: "jwt",
+    },
+    pages: { signIn: "signin" },
+    providers: [
+        GoogleProvider({
+            clientId: process.env.GOOGLE_CLIENT_ID!,
+            clientSecret: process.env.GOOGLE_CLIENT_SECRET!,
+        }),
+    ],
 };
