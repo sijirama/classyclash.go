5f1d36666fdd1e680a2b6f655c056b4b5ef9622d
sijibomi <gbemilesanmi@gmail.com>
2023-12-27 04:12:48 +0100

    again and again

---
+++ TODO.md
@@ -0,0 +1,2 @@
+[] https://almacommunities.vercel.app/auth/terms
+[] https://almacommunities.vercel.app/auth/privacy
--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -1,7 +1,8 @@
-import { NextAuthOptions } from "next-auth";
+import { NextAuthOptions, getServerSession } from "next-auth";
 import { PrismaAdapter } from "@next-auth/prisma-adapter";
 import { db } from "./db";
 import GoogleProvider from "next-auth/providers/google";
+import { nanoid } from "nanoid";
 
 export const authOptions: NextAuthOptions = {
     adapter: PrismaAdapter(db),
@@ -15,4 +16,47 @@ export const authOptions: NextAuthOptions = {
             clientSecret: process.env.GOOGLE_CLIENT_SECRET!,
         }),
     ],
+    callbacks: {
+        async session({ token, session }) {
+            if (token && session.user) {
+                session.user.id = token.id;
+                session.user.name = token.name;
+                session.user.email = token.email;
+                session.user.image = token.picture;
+                session.user.username = token.username;
+            }
+            return session;
+        },
+        async jwt({ token, user }) {
+            const dbUser = await db.user.findFirst({
+                where: {
+                    email: token.email,
+                },
+            });
+            if (!dbUser) {
+                token.id = user!.id;
+                return token;
+            }
+            if (!dbUser.username) {
+                await db.user.update({
+                    where: { id: dbUser.id },
+                    data: {
+                        username: nanoid(10),
+                    },
+                });
+            }
+            return {
+                id: dbUser.id,
+                name: dbUser.name,
+                email: dbUser.email,
+                picture: dbUser.image,
+                username: dbUser.username,
+            };
+        },
+        redirect() {
+            return "/";
+        },
+    },
 };
+
+export const getUserSession = () => getServerSession(authOptions);
